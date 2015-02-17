965e456067bae49c0246d852d80aa8e110b078fd
moonorblue <moonorblue@gmail.com>
2015-02-18 00:51:28 +0800

    Initial Commit

---
+++ NewYearRedEnv/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewYearRedEnv
+//
+//  Created by KaeJer Cho on 2015/2/18.
+//  Copyright (c) 2015年 KaeJer Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewYearRedEnv/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewYearRedEnv
+//
+//  Created by KaeJer Cho on 2015/2/18.
+//  Copyright (c) 2015年 KaeJer Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
