bd21299d21f7f4f0a033685a74eea5c6f7cfe413
moonorblue <moonorblue@gmail.com>
2015-02-18 12:26:20 +0800

    prototype beta 1

--- NewYearRedEnv/ViewController.m
+++ NewYearRedEnv/ViewController.m
@@ -1,3 +1,5 @@
+
+
 //
 //  ViewController.m
 //  NewYearRedEnv
@@ -9,19 +11,49 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
 @end
-
+NSMutableArray *imgArray;
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+  [super viewDidLoad];
+  [self initArray];
+
+  // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)ButtonClick:(id)sender {
+  [self shuffle:imgArray];
+  [self showHeadViewWithArray:imgArray];
+}
+
+- (void)shuffle:(NSMutableArray *)ary {
+  NSUInteger count = [ary count];
+  for (NSUInteger i = 0; i < count; ++i) {
+    NSInteger remainingCount = count - i;
+    NSInteger exchangeIndex = i + arc4random_uniform((u_int32_t)remainingCount);
+    [ary exchangeObjectAtIndex:i withObjectAtIndex:exchangeIndex];
+  }
+}
+- (void)initArray {
+  imgArray = [[NSMutableArray alloc] init];
+  int x;
+  for (x = 1; x <= 10; x++) {
+    NSString *str =
+        [[NSString stringWithFormat:@"%d", x] stringByAppendingString:@".png"];
+    UIImage *img = [UIImage imageNamed:str];
+    [imgArray addObject:img];
+  }
+}
+
+- (void)showHeadViewWithArray:(NSMutableArray *)ary {
+  for (id i in ary) {
+    self.headView.image = i;
+  }
+}
 @end
